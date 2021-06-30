91c5ee6c125607b949f89fa3140299b31df43ebf
Gergely Izsak <izsak.gera@gmail.com>
2021-06-30 11:10:22 +0200

    basic

---
+++ Basic1/script.js
@@ -0,0 +1,41 @@
+let car1 = {
+    brand: "Tesla",
+    color: "blue",
+    type: "electronic",
+    fullSpecs: function() {
+return "My car is" + car1.brand + ". The color is " + this.color + ". It is " + this.type + " type."}
+}
+
+let car2 = {
+    brand: "Nissan",
+    color: "purple",
+    type: "benzin",
+    fullSpecs: function() {
+// return "My car is" + this.brand + ". The color is " + this.color + ". It is " + this.type + " type."}
+}
+
+let car3 = {
+    brand: "Audi",
+    color: "red",
+    type: "diesel",
+    fullSpecs: function() {
+// return "My car is" + this.brand + ". The color is " + this.color + ". It is " + this.type + " type."}
+}
+
+console.log(car1.fullSpecs)
+
+
+
+
+
+
+
+
+
+let animal = {
+
+}
+
+let person = {
+
+}
